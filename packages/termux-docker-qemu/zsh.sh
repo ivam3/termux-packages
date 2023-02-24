3002ca1da700361d5ebca3b14ef34dd3b8cfbb68
ivam3 <ivam3.bh@gmail.com>
2023-02-23 20:53:46 -0600

    update termux-docker-qemu

--- packages/termux-docker-qemu/zsh.sh
+++
@@ -1,6 +0,0 @@
-apk update
-apk upgrade
-apk add docker
-service docker start
-rc-update add docker
-rm /etc/profile.d/zsh.sh
--- packages/termux-docker-qemu/zshrc.sh
+++
@@ -1,5 +0,0 @@
-[[ -e /etc/resolv.conf ]] || { touch /etc/resolv.conf;}
-chk=$(grep -oE "8.8.8.8" /etc/resolv.conf)
-if [[ -z $chk ]]; then
-  echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-fi
