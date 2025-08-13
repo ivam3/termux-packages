8e89f68f7e4711cb9b9612388ae483129084f0a9
ivam3 <ivam3.bh@gmail.com>
2025-08-13 04:05:34 -0600

    update MSF

--- updebs.sh
+++ updebs.sh
@@ -1,7 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+#MOVE LFS OUT TO REPO 
+mv openjdk-21_21.1.1-1_aarch64.deb ../
+
 #ACTUALIZA DEB FILES AL ARBOL DE DIRECTORIOS
 termux-apt-repo . . stable extras
+
+#RETURN LFS FILES 
+mv ../openjdk-21_21.1.1-1_aarch64.deb .
 
 #CREAMOS EL FILE Packages.gz
 #pushd dists/stable/extras/binary-all/
