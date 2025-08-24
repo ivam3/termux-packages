905e5ea35a43b7d719f995749fc1771aebdd4516
ivam3 <ivam3.bh@gmail.com>
2025-08-23 18:33:16 -0600

    fix E: not such... argv

--- updebs.sh
+++ updebs.sh
@@ -1,12 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 #MOVE LFS OUT TO REPO 
+echo "Saving LFS files to repo directory..."
 mv openjdk-21_21.1.1-1_aarch64.deb ../
 
 #ACTUALIZA DEB FILES AL ARBOL DE DIRECTORIOS
 termux-apt-repo . . stable extras
 
 #RETURN LFS FILES 
+echo "Recovering LFS files to repo directory..."
 mv ../openjdk-21_21.1.1-1_aarch64.deb .
 
 #CREAMOS EL FILE Packages.gz
