a0405ccb79b6ca3b449194861d4f1ac2a2b93d26
ivam3 <ivam3.bh@gmail.com>
2026-07-23 18:14:47 -0600

    fix E: proot-distro overwrited

--- packages/termux-docker-qemu/alpineX11.sh
+++ packages/termux-docker-qemu/alpineX11.sh
@@ -1,14 +1,17 @@
 echo "Installing x11 dependencies and setting file ..."
-setup-xorg-base xfce4 xfce4-terminal dbus-x11 thunar-volman
+setup-xorg-base
+apk add xfce4 xfce4-terminal dbus-x11 thunar-volman
 rc-service dbus start
 rc-update add dbus
 rc-service udev start
 rc-update add udev
 apk add lightdm-gtk-greeter
 rc-update add lightdm
-apk add polkit consolekit2 firefox
+apk add polkit consolekit2
+echo "autologin-user=root" >> /etc/lightdm/lightdm.conf
+apk add firefox
 echo "Installation finished."
 echo "The system will shutdown"
-echo "Re-start it with 'termux-docker-qemu alpine x11' for a graphical enviroment"
+echo "Re-start it with: termux-docker-qemu alpine x11 sdl"
 echo "Press ENTER to continue."
 poweroff
